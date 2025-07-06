53f24689a261eb1f11f15e484dbcf83b913d6cb2
Tagir Fayrushin <fayrushintagir@gmail.com>
2025-07-06 11:26:33 +0300

    init

---
+++ githubActions/githubActions/githubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubActionsApp.swift
+//  githubActions
+//
+//  Created by Тагир Файрушин on 06.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct githubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
